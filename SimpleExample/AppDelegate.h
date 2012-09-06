61ff3c606734f428f6afa8b74cd14f15661f35a7
Matt Luedke <mluedke2@unknown7cd1c3963150.att.net>
2012-09-06 14:24:27 -0500

    Initial Commit

---
+++ SimpleExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SimpleExample
+//
+//  Created by Matt Luedke on 9/6/12.
+//  Copyright (c) 2012 Matt Luedke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SimpleExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleExample
+//
+//  Created by Matt Luedke on 9/6/12.
+//  Copyright (c) 2012 Matt Luedke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
